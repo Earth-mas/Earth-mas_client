04e2066267af8a88170d1838d28502f97bcf034d
KKangdaa <97223303+KKangdaa@users.noreply.github.com>
2022-06-06 19:47:39 +0900

    feature-#2

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -1,2 +1,3 @@
 export { ReactComponent as Logo } from './logo/logo-main.svg';
+export { ReactComponent as LogoWhite } from './logo/logo-main-w.svg';
 export { ReactComponent as LogoIcon } from './logo/logo-icon.svg';
