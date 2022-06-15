92dd4ee13d0e24361076149b1216ad7506541c07
KKangdaa <97223303+KKangdaa@users.noreply.github.com>
2022-06-16 03:09:47 +0900

    Feature: [#61] apply support list page API

--- src/components/commons/datePicker/index.tsx
+++ src/components/commons/datePicker/index.tsx
@@ -7,7 +7,7 @@ import { FontFamily, FontSize } from 'styles/FontStyles';
 import { ko } from 'date-fns/esm/locale';
 
 interface IDatePickerProps {
-  onChange: (
+  onChangeDate: (
     date: Date | null,
     event: SyntheticEvent<any, Event> | undefined,
   ) => void;
@@ -15,7 +15,7 @@ interface IDatePickerProps {
   date: Date | null | undefined;
 }
 
-export default function DatePicker01() {
+export default function DatePicker01(props: IDatePickerProps) {
   const [startDate, setStartDate] = useState<Date | null>(new Date());
 
   const onChangeDate = (date: Date | null) => {
@@ -49,7 +49,7 @@ export default function DatePicker01() {
         // selectsRange={true}
         startDate={props.date}
         // endDate={endDate}
-        onChange={props.onChange}
+        onChange={props.onChangeDate}
         // dayClassName={date =>
         //   getDayName(createDate(date)) === '토'
         //     ? 'saturday'
@@ -71,46 +71,47 @@ const Wrapper = styled.div`
     .react-datepicker {
       border-top-right-radius: 0;
 
-    .saturday {
-      color: rgb(0, 0, 255) !important;
-    }
+      .saturday {
+        color: rgb(0, 0, 255) !important;
+      }
 
-    &__input-container {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      margin-bottom: 25px;
+      &__input-container {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 25px;
 
-      input {
-        border-radius: 8px;
-        border: 1px solid ${Colors.B60};
-        padding: 0px 20px;
-        width: 100%;
-        height: 48px;
-        font-family: ${FontFamily.MEDIUM};
-        font-size: ${FontSize.MEDIUM_C};
-        color: ${Colors.B80};
-
-        &::placeholder {
-          color: ${Colors.B60};
-        }
-        &:focus {
-          border: 1px solid ${Colors.SUB1};
+        input {
           border-radius: 8px;
-          color: ${Colors.B100};
+          border: 1px solid ${Colors.B60};
+          padding: 0px 20px;
+          width: 100%;
+          height: 48px;
+          font-family: ${FontFamily.MEDIUM};
+          font-size: ${FontSize.MEDIUM_C};
+          color: ${Colors.B80};
+
+          &::placeholder {
+            color: ${Colors.B60};
+          }
+          &:focus {
+            border: 1px solid ${Colors.SUB1};
+            border-radius: 8px;
+            color: ${Colors.B100};
+          }
         }
       }
     }
-  }
-  .react-datepicker-popper {
-    padding-top: 0px;
-    .react-datepicker__triangle {
-      ::before {
-        border-bottom-color: ${Colors.B40};
-        left: -12px;
-      }
-      ::after {
-        left: -12px;
+    .react-datepicker-popper {
+      padding-top: 0px;
+      .react-datepicker__triangle {
+        ::before {
+          border-bottom-color: ${Colors.B40};
+          left: -12px;
+        }
+        ::after {
+          left: -12px;
+        }
       }
     }
   }
--- src/components/units/support/list/SupportList.container.tsx
+++ src/components/units/support/list/SupportList.container.tsx
@@ -1,5 +1,41 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { supportRoute } from 'utils/APIRoutes';
 import SupportListUI from './SupportList.presenter';
+import { ISupportListProps } from './SupportList.types';
 
 export default function SupportList() {
-  return <SupportListUI />;
+  const navigate = useNavigate();
+  const [list, setList] = useState<ISupportListProps[]>([]);
+
+  /* const supportFindDday = async () => {
+    await axios
+      .get(`${supportRoute}/finddcs`)
+      .then(res => {
+        console.log(res);
+        setList(res.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+  supportFindDday(); */
+
+  useEffect(() => {
+    const supportFindDcs = async () => {
+      await axios
+        .get(`${supportRoute}/finddcs`)
+        .then(res => {
+          console.log(res);
+          setList(res.data);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    };
+    supportFindDcs();
+  }, []);
+
+  return <SupportListUI list={list} />;
 }
