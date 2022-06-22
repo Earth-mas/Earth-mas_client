832500e078d6efce4b16765cde0ec9d77fde8693
minimin123 <kmmin727@gmail.com>
2022-06-22 23:43:56 +0900

    build

--- src/hooks/useSetUser.tsx
+++ src/hooks/useSetUser.tsx
@@ -1,5 +1,5 @@
 import store from 'storejs';
-import axios from 'axios';
+// import axios from 'axios';
 import { useSetRecoilState } from 'recoil';
 import { userState } from 'recoil/user';
 import axiosApiInstance from 'commons/utils/axiosInstance';
@@ -9,32 +9,32 @@ export default function useSetUser() {
   const accessToken = store.get('accessToken');
 
   // if 부분은 배포시 지워야 함.
-  if (accessToken) {
-    axiosApiInstance
-      .get('user/me', {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      })
-      // axios
-      //   .get('https://earth-mas.shop/server/user/me', {
-      //     headers: {
-      //       Authorization: `Bearer ${accessToken}`,
-      //     },
-      //   })
-      .then(res => {
-        setUser({
-          id: res.data.id,
-          name: res.data.name,
-          email: res.data.email,
-          url: res.data.url,
-          addressnumber: res.data.addressnumber,
-          address1: res.data.address1,
-          address2: res.data.address2,
-        });
-      })
-      .catch(error => {
-        alert(error.response.data.message);
+  // if (accessToken) {
+  axiosApiInstance
+    .get('user/me', {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    })
+    // axios
+    //   .get('https://earth-mas.shop/server/user/me', {
+    //     headers: {
+    //       Authorization: `Bearer ${accessToken}`,
+    //     },
+    //   })
+    .then(res => {
+      setUser({
+        id: res.data.id,
+        name: res.data.name,
+        email: res.data.email,
+        url: res.data.url,
+        addressnumber: res.data.addressnumber,
+        address1: res.data.address1,
+        address2: res.data.address2,
       });
-  }
+    })
+    .catch(error => {
+      alert(error.response.data.message);
+    });
+  // }
 }
