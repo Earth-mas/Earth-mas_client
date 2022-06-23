758f7707d3d255d02d1ce43222bf2b71072fa334
minimin123 <kmmin727@gmail.com>
2022-06-23 19:28:29 +0900

    fix:logout error build

--- src/hooks/useSetUser.tsx
+++ src/hooks/useSetUser.tsx
@@ -9,35 +9,34 @@ export default function useSetUser() {
   const setUser = useSetRecoilState(userState);
   const accessToken = store.get('accessToken');
   useEffect(() => {
-    // if 부분은 배포시 지워야 함.
-    // if (accessToken) {
-    axiosApiInstance
-      .get('user/me', {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      })
-      // axios
-      //   .get('https://earth-mas.shop/server/user/me', {
-      //     headers: {
-      //       Authorization: `Bearer ${accessToken}`,
-      //     },
-      //   })
-      .then(res => {
-        setUser({
-          id: res.data.id,
-          name: res.data.name,
-          email: res.data.email,
-          url: res.data.url,
-          addressnumber: res.data.addressnumber,
-          address1: res.data.address1,
-          address2: res.data.address2,
-          phone: res.data.phone,
+    if (accessToken) {
+      axiosApiInstance
+        .get('user/me', {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        })
+        // axios
+        //   .get('https://earth-mas.shop/server/user/me', {
+        //     headers: {
+        //       Authorization: `Bearer ${accessToken}`,
+        //     },
+        //   })
+        .then(res => {
+          setUser({
+            id: res.data.id,
+            name: res.data.name,
+            email: res.data.email,
+            url: res.data.url,
+            addressnumber: res.data.addressnumber,
+            address1: res.data.address1,
+            address2: res.data.address2,
+            phone: res.data.phone,
+          });
+        })
+        .catch(error => {
+          alert(error.response.data.message);
         });
-      })
-      .catch(error => {
-        alert(error.response.data.message);
-      });
-    // }
+    }
   }, []);
 }
