2a9fc6fd91c3eadfe74c6cca364e57f661e6f7a9
KKangdaa <97223303+KKangdaa@users.noreply.github.com>
2022-07-21 18:41:22 +0900

    Chat 수정 및 datePicker 수정

--- src/components/units/support/detail/SupportDetail.presenter.tsx
+++ src/components/units/support/detail/SupportDetail.presenter.tsx
@@ -19,7 +19,7 @@ export default function SupportDetailUI(props: ISupportDetailUIProps) {
   const userInfo = useRecoilValue(userState);
 
   return (
-    <>
+    <S.Wrapper>
       {props.modal && (
         <Modal>
           <AlertModal
@@ -32,83 +32,81 @@ export default function SupportDetailUI(props: ISupportDetailUIProps) {
           />
         </Modal>
       )}
-      <S.Wrapper>
-        <S.FirstSection>
-          <Slide slide="sub" banner={props.data?.url?.split(',')} />
-          <S.MainContent percent={props.percent}>
-            <div className="rowWrap">
-              <div className="dDay">
-                {props.leftDay > 0
-                  ? `Done`
-                  : props.leftDay < 0
-                  ? `D${props.leftDay}`
-                  : props.leftDay === 0 && 'D-day'}
+      <S.FirstSection>
+        <Slide slide="sub" banner={props.data?.url?.split(',')} />
+        <S.MainContent percent={props.percent}>
+          <div className="rowWrap">
+            <div className="dDay">
+              {props.leftDay > 0
+                ? `Done`
+                : props.leftDay < 0
+                ? `D${props.leftDay}`
+                : props.leftDay === 0 && 'D-day'}
+            </div>
+            {props.data?.user?.id === userInfo.id && (
+              <Dropdown03 page="support" openModal={props.openModal} />
+            )}
+          </div>
+
+          <div className="title">{props.data?.title}</div>
+
+          <div className="percent">{props.percent}%</div>
+          <div className="graph">
+            <div className="participate"></div>
+          </div>
+
+          <div className="goal">
+            <p>{getPrice(props.data?.wishamount)}원 목표</p>
+            <p>{getPrice(props.data?.currentamount)}원</p>
+          </div>
+
+          <div className="user_button">
+            <div className="user">
+              <div className="userImg">
+                <img
+                  src={props.data?.user?.url}
+                  onError={e => {
+                    e.currentTarget.src = '/images/profileDefault.png';
+                  }}
+                />
               </div>
-              {props.data?.user?.id === userInfo.id && (
-                <Dropdown03 page="support" openModal={props.openModal} />
-              )}
+              <p className="userName">{props.data?.user?.name}</p>
             </div>
+            {props.data?.user?.id !== userInfo.id && (
+              <ChatButton userInfo={props.data?.user} content="문의하기" />
+            )}
+          </div>
 
-            <div className="title">{props.data?.title}</div>
+          <Line />
 
-            <div className="percent">{props.percent}%</div>
-            <div className="graph">
-              <div className="participate"></div>
-            </div>
+          <p className="subText">
+            기부하신 금액은 수수료 없이 <S.Sub1>100% 전달</S.Sub1>
+            됩니다
+          </p>
 
-            <div className="goal">
-              <p>{getPrice(props.data?.wishamount)}원 목표</p>
-              <p>{getPrice(props.data?.currentamount)}원</p>
-            </div>
+          <Link to={'payment'}>
+            <ContainedButton01 content="기부하기" color="main" />
+          </Link>
+        </S.MainContent>
+      </S.FirstSection>
 
-            <div className="user_button">
-              <div className="user">
-                <div className="userImg">
-                  <img
-                    src={props.data?.user?.url}
-                    onError={e => {
-                      e.currentTarget.src = '/images/profileDefault.png';
-                    }}
-                  />
-                </div>
-                <p className="userName">{props.data?.user?.name}</p>
-              </div>
-              {props.data?.user?.id !== userInfo.id && (
-                <ChatButton userInfo={props.data?.user} content="문의하기" />
-              )}
-            </div>
+      <Line />
 
-            <Line />
+      <S.SecondSection>
+        <S.Contents
+          dangerouslySetInnerHTML={{
+            __html: Dompurify.sanitize(props.data?.description),
+          }}
+        />
 
-            <p className="subText">
-              기부하신 금액은 수수료 없이 <S.Sub1>100% 전달</S.Sub1>
-              됩니다
-            </p>
+        <ParticipationListContainer />
+      </S.SecondSection>
 
-            <Link to={'payment'}>
-              <ContainedButton01 content="기부하기" color="main" />
-            </Link>
-          </S.MainContent>
-        </S.FirstSection>
+      <Line />
 
-        <Line />
-
-        <S.SecondSection>
-          <S.Contents
-            dangerouslySetInnerHTML={{
-              __html: Dompurify.sanitize(props.data?.description),
-            }}
-          />
-
-          <ParticipationListContainer />
-        </S.SecondSection>
-
-        <Line />
-
-        <S.ThirdSection>
-          <CommentContainer />
-        </S.ThirdSection>
-      </S.Wrapper>
-    </>
+      <S.ThirdSection>
+        <CommentContainer />
+      </S.ThirdSection>
+    </S.Wrapper>
   );
 }
--- src/components/units/support/detail/participationList/ParticipationList.container.tsx
+++ src/components/units/support/detail/participationList/ParticipationList.container.tsx
@@ -5,34 +5,43 @@ import { supportRoute } from 'utils/APIRoutes';
 import { useParams } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 import { ParticipationList } from './ParticipationList.presenter';
-import { IItem, IpaymentListElement } from './ParticipationList.types';
-import { useState } from 'react';
+import { IParticipationList, ISupported } from './ParticipationList.types';
+import { useEffect, useState } from 'react';
 
-let index = 0;
+let index = 1;
 
 export const ParticipationListContainer = () => {
   const { id } = useParams();
 
-  const [list, setList] = useState<IItem[]>([]);
+  const [list, setList] = useState<ISupported[]>([]);
 
-  const addList = () => {
-    const items = [];
-    for (let i = 0; i < 10; i++) {
-      items.push({
-        idx: index * 10 + i,
-        name: `hello` + i,
-      });
-    }
-    setList([...list, ...items]);
-    index++;
-  };
-
-  const { data: paymentList } = useQuery('Support', async () => {
+  const { data: paymentList } = useQuery('supported', async () => {
     const { data } = await axios.post(`${supportRoute}/supported`, {
       id: id,
     });
     return data;
   });
+
+  const addList = () => {
+    const items: ISupported[] = [];
+
+    for (let i = 0; i < 10 && index * 10 + i < paymentList.length; i++) {
+      items?.push(paymentList[index * 10 + i]);
+    }
+    setList([...list, ...items]);
+
+    index++;
+  };
+
+  useEffect(() => {
+    setList(
+      paymentList?.filter((el: IParticipationList, index: number) => {
+        if (index < 10) return el;
+      }),
+    );
+  }, [paymentList]);
+
+  console.log(list);
 
   return (
     <S.ParticipationList>
@@ -42,13 +51,22 @@ export const ParticipationListContainer = () => {
         <p className="totalCount">
           총 <S.Sub1>{paymentList?.length}</S.Sub1>건이 기부되었습니다.
         </p>
-        {paymentList?.map((el: IpaymentListElement) => (
-          <ParticipationList key={uuidv4()} el={el} />
-        ))}
 
-        <p className="more" onClick={addList}>
-          더보기
-        </p>
+        {list
+          ?.filter(el => {
+            return el;
+          })
+          ?.map((el: ISupported) => (
+            <ParticipationList key={uuidv4()} el={el} />
+          ))}
+
+        {paymentList?.length === list?.length ? (
+          ''
+        ) : (
+          <p className="more" onClick={addList}>
+            더보기
+          </p>
+        )}
       </div>
     </S.ParticipationList>
   );
